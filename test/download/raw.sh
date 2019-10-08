f296e2aae4d0ca4a65fe849a77e173437610b617
RangerUFO <ufownl@gmail.com>
2019-10-08 18:46:23 +0800

    Adjust the project structure of server

--- test/download/raw.sh
+++ test/download/raw.sh
@@ -2,6 +2,6 @@
 # 下载原始视频
 
 curl -G --data-urlencode "id=$1" \
-     "http://localhost:2980/hls_vod/download/raw"
+     "http://localhost:2980/hls_vod/api/download/raw"
 
 # 返回: 文件数据二进制流
--- test/upload/raw.sh
+++ test/upload/raw.sh
@@ -2,6 +2,6 @@
 # 上传原始视频
 
 curl -F "filename=@$1" \
-     "http://localhost:2980/hls_vod/upload/raw"
+     "http://localhost:2980/hls_vod/api/upload/raw"
 
 # 返回: ["视频ID", ...]
