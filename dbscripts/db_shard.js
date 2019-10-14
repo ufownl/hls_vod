ce6fdba1786274c0c711eadc8acbd9fd9c91fe60
RangerUFO <ufownl@gmail.com>
2019-10-14 19:20:21 +0800

    Add the profile field to the shard key of segments collection

--- dbscripts/db_shard.js
+++ dbscripts/db_shard.js
@@ -1,4 +1,4 @@
-sh.shardCollection(db + ".segments", {video: 1})
+sh.shardCollection(db + ".segments", {video: 1, profile: 1}, true)
 
 sh.shardCollection(db + ".fs.raw.files", {_id: 1})
 sh.shardCollection(db + ".fs.raw.chunks", {files_id: 1, n: 1}, true)
