b17be2c04ff568fb7f1657544badf6172acd39ef
RangerUFO <ufownl@gmail.com>
2019-10-14 18:19:53 +0800

    Add the database sharding script

---
+++ dbscripts/db_shard.js
@@ -0,0 +1,10 @@
+sh.shardCollection(db + ".segments", {video: 1})
+
+sh.shardCollection(db + ".fs.raw.files", {_id: 1})
+sh.shardCollection(db + ".fs.raw.chunks", {files_id: 1, n: 1}, true)
+
+sh.shardCollection(db + ".fs.cover.files", {_id: 1})
+sh.shardCollection(db + ".fs.cover.chunks", {files_id: 1, n: 1}, true)
+
+sh.shardCollection(db + ".fs.segment.files", {_id: 1})
+sh.shardCollection(db + ".fs.segment.chunks", {files_id: 1, n: 1}, true)
