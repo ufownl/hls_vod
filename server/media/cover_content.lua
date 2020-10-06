28bc173087351a1b4164c0047d59cefbabab8424
RangerUFO <ufownl@gmail.com>
2020-10-06 23:10:42 +0800

    Stream the data of the files in the database

--- server/media/cover_content.lua
+++ server/media/cover_content.lua
@@ -1,12 +1,13 @@
 local file = vod_core.open_cover(ngx.var.cover)
-local content, err = file:slurp()
-if not content then
-  ngx.log(ngx.ERR, "mongodb error: ", err)
-  ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
-end
 ngx.ctx.cache_control = {
   public = true,
   max_age = 30 * 24 * 3600
 }
 ngx.header["Content-Type"] = "image/jpeg"
-ngx.print(content)
+while true do
+  local chunk = file:read()
+  if not chunk then
+    break
+  end
+  ngx.print(chunk)
+end
--- server/media/segment_content.lua
+++ server/media/segment_content.lua
@@ -1,12 +1,13 @@
 local file = vod_core.open_segment(ngx.var.segment)
-local content, err = file:slurp()
-if not content then
-  ngx.log(ngx.ERR, "mongodb error: ", err)
-  ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
-end
 ngx.ctx.cache_control = {
   public = true,
   max_age = 30 * 24 * 3600
 }
 ngx.header["Content-Type"] = "video/mp2t"
-ngx.print(content)
+while true do
+  local chunk = file:read()
+  if not chunk then
+    break
+  end
+  ngx.print(chunk)
+end
