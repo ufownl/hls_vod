34ddc0bfc0c4c2fe37254aa4e3b6f254ad63d2d9
RangerUFO <ufownl@gmail.com>
2022-09-15 21:10:34 +0800

    Add copyright notices to source files

--- server/api/download/raw_content.lua
+++ server/api/download/raw_content.lua
@@ -1,3 +1,21 @@
+--[[
+--  HLS vod, video-on-demand server using HLS protocol.
+--  Copyright (C) 2022  RangerUFO
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+--]]
+
 local args = ngx.req.get_uri_args(1)
 local file = vod_core.open_raw(args.id)
 ngx.header["Content-Type"] = "application/octet-stream"
